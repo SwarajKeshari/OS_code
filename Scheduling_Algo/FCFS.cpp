536954ebaa9d8852ecae0ac0d479269ed064c4f0
Swaraj Keshari <147552314+SwarajKeshari@users.noreply.github.com>
2026-05-02 12:52:30 +0530

    complete FCFS Algo

--- Scheduling_Algo/FCFS.cpp
+++ Scheduling_Algo/FCFS.cpp
@@ -1,12 +1,46 @@
-#include <bit/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-void findWaitingTime(int processers[], int n, int bt[], int wt[]){
-  wt[0]=0;
+void findWaitingTime(int processes[], int n, int bt[], int wt[]) {
+    wt[0] = 0;
 
-  for(int i=1; i<n; i++){
-    wt[i] = bt[i-1] + wt[i-1];
-  }
+    for(int i = 1; i < n; i++) {
+        wt[i] = bt[i-1] + wt[i-1];
+    }
+}
 
-  
+void findTurnAroundTime(int processes[], int n, int bt[], int wt[], int tat[]) {
+    for(int i = 0; i < n; i++) {
+        tat[i] = bt[i] + wt[i];
+    }
+}
+
+void findavgTime(int processes[], int n, int bt[]) {
+    int wt[n], tat[n], total_wt = 0, total_tat = 0;
+
+    findWaitingTime(processes, n, bt, wt);
+    findTurnAroundTime(processes, n, bt, wt, tat);
+
+    cout << "Processes  Burst Time  Waiting Time  Turn Around Time\n";
+
+    for(int i = 0; i < n; i++) {
+        total_wt += wt[i];
+        total_tat += tat[i];
+
+        cout << " " << processes[i] << "\t\t" << bt[i] << "\t\t" << wt[i] << "\t\t" << tat[i] << endl;
+    }
+
+    cout << "\nAverage waiting time = " << (float)total_wt / n;
+    cout << "\nAverage turn around time = " << (float)total_tat / n;
+}
+
+int main() {
+    int processes[] = {1,2,3};
+    int n = sizeof(processes) / sizeof(processes[0]);
+
+    int burst_time[] = {10,5,8};
+
+    findavgTime(processes, n, burst_time);
+
+    return 0;
 }
